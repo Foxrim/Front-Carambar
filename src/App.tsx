3c7a9f6a3c6898431ff1121c5efa1c8f81328040
Foxrim <Flavien085@gmail.com>
2025-05-18 19:22:14 +0200

    [feat] - Questionnaire foncitonnel

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import { useState } from "react";
 import styles from "./styles/App.module.css";
+import logo from "/images/logo-rose.png";
+import Quizz from "./quizz/Quizz";
 
 function App() {
-  const [joke, setJoke] = useState();
-
-  const getRandomJoke = async () => {
-    const response = await fetch('https://back-carambar.onrender.com/api/v1/blagues');
-    const data = await response.json();
-    setJoke(data);
-  };
-
   return (
     <>
       <div className={styles.background}>
-        <button onClick={getRandomJoke} type="button">Une autres ?</button>
-        {joke && <p>{joke}</p>}
+        <figure className={styles.jokePlace}>
+          <img className={styles.logo} src={logo} alt="Logo de la marque carambar&co" />
+          <Quizz/>
+        </figure>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/quizz/Quizz.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import styles from "./styles/Quizz.module.css";
+import etoile from "/images/etoile.svg";
+import eclaboussure from "/images/mini-eclaboussure.svg";
+
+type Answers = {
+  text_answer: string;
+};
+
+type JokeProps = {
+  text_question: string;
+  answers: Answers[];
+};
+
+export default function Quizz() {
+  const [joke, setJoke] = useState<JokeProps | null>(null);
+  const [isShow, setIsShow] = useState<boolean>(false);
+
+  useEffect(() => {
+    getRandomJoke();
+  }, []);
+
+  const getRandomJoke = async () => {
+    setIsShow(false);
+    const response = await fetch(
+      "http://localhost:3008/api/v1/blagues/random",
+      {
+        credentials: "include",
+      }
+    );
+    const data = await response.json();
+    setJoke(data);
+  };
+
+  const showResponse = () => {
+    setIsShow(true);
+  }
+
+  return (
+    <div className={styles.whiteboard}>
+      <h2>AS TU LA RÉPONSE ?</h2>
+      {joke && <h3>{joke.text_question}</h3>}
+      {!isShow ? (
+        <button onClick={showResponse} type="button">Voir la réponse</button>
+      ) : (
+        joke && <p>{joke.answers[0].text_answer}</p>
+      )}
+
+      <button onClick={getRandomJoke} type="button">
+        Une autres ?
+      </button>
+
+      <img className={styles.star} src={etoile} alt="etoile" />
+      <img className={styles.mini} src={eclaboussure} alt="mini eclaboussure" />
+    </div>
+  );
+}
